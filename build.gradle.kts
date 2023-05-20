085825f0e6e9422276d169eea08e7c29df500e24
Amaury Rousseau <kinrar@kinrar.io>
2023-05-20 18:57:29 +0200

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly.
    
    Paper Changes:
    bcd8dc2c1 Use the spigot config for mangrove propagule / torchflower growth (#8597)
    b6817605c Fix note block sound property on player heads (#9158)
    a61b4845f Fix MC-224454 (#9195)
    4ba5aacc3 More accurate isInOpenWater impl (#9194)
    5b66d33a7 Call EntityChangeBlockEvent for frogs egg (#9213)
    f8603a46a Avoid Lazy Initialization for Enum Fields (#9210)
    57fdc668f Undeprecate Server#getMap (#9185)
    ab7bc3fb3 Remove javadoc that doesn't apply to Paper (#9206)
    9daa019d6 Added wither vanilla invulnerability API (#9124)
    1edfefdef Add option to flush region files on save (#9149)
    efd47e3a6 Updated Upstream (Bukkit/CraftBukkit/Spigot) (#9188)
    688409279 Fix lightning being able to hit spectators (#9193)
    1d4c7806f Fix BlockPosition#offset(double,double,double) (#9190)
    813d0399f Make sure the player didn't disconnect when processing commands
    9846d0d89 Fix some NPE for EntityCompostItemEvent (#9169)
    83da4b610 Fix DamageSource for Falling Blocks (#9175)
    4f9575eed Break redstone on top of trap doors early
    bcec5b11e Fixed large packet handling (#9147)
    f4ce997a4 Add Mob Experience reward API (#9122)
    2cccbbe1f [ci skip] Update Gradle plugins and wrapper (#9140)
    936205bea Disable allowListing before received from client (#9167)
    5c4671a9b Fix LegacyPluginLoader dependency registration (#9165)
    8e3009e0d Resolve Plugin Dependency Issues, Improve PluginLoading Compat, Small Loading Issues (#9129)
    f7717c371 Fix server ignoring fallback for translatable components (#9134)
    beed22d33 Undo making JavaPlugin#logger field public (#9125)
    dd216c1f0 Fix Inconsistencies with Paper Plugin Names (#9098)
    6d2121efd Removes some references to deprecated methods/types (#9043)
    23a443eea Fix demo flag not enabling demo mode (#9103)
    8d040fad9 Updated Upstream (Bukkit/CraftBukkit)
    bd35d605e [ci-skip] Fix missing closing quote (#9126)
    e33879360 Optimize hoppers by not trying to merge full items. (#9110)
    5ffdff882 Fix a couple of bed bugs (#9112)
    e81192739 Revert "Resolve Plugin Dependency Issues, Improve PluginLoading Compat, Small Loading Issues (#9068)"
    8be7a60b7 Fix getBrightness and getRawBrightness throwing exception in BlockStateListPopulator (#9111)
    ab72b123e Update Adventure to 4.13.1 (#9113)
    058d7c1aa Updated Upstream (Bukkit/CraftBukkit/Spigot)
    b626528da Updated Upstream (Bukkit/CraftBukkit)
    f8d2f823d Resolve Plugin Dependency Issues, Improve PluginLoading Compat, Small Loading Issues (#9068)
    6a7fef0e4 Allow entity effect changes off the main thread for worldgen (#8942)
    f8c01122e [ci skip] add missing labels to project status map (#9106)
    9cda28480 Updated Upstream (Bukkit/CraftBukkit)
    b5ce6e3dc Updated Upstream (Bukkit/CraftBukkit/Spigot) (#9104)
    29b17a892 Updated Upstream (Bukkit/CraftBukkit/Spigot) (#9088)
    ad6c14c62 Fix unknown cmd msg for mc namespaced cmds (#9059)
    e9f6e7ae8 Properly resync player on Player#setPlayerProfile (#9080)
    f5677aa47 Fix TPS command output (#9091)
    3c8c22323 Fix some broken block place logic (#9094)
    dab4864a1 [ci skip] add workflow for auto-managing issue board (#8958)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.Git
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.3"
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.5"
 }
 
 val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
